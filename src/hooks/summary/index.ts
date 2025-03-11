cef4bec486c3e5337479c61bf59ad6a0873c7f19
Lordsisodia <130863457+Lordsisodia@users.noreply.github.com>
2025-03-11 04:50:41 +0000

    Delete src/hooks/summary directory

--- src/hooks/summary/index.ts
+++
@@ -1,9 +0,0 @@
-
-// Export the main hook and related utilities from a single file
-export { useAiDailySummary } from '../useAiDailySummary';
-export { 
-  isFallbackSummary, 
-  getSummaryGenerationLabel,
-  hasEnhancedAnalysis
-} from '@/utils/summary-utils';
-export type { DailySummaryData } from '@/types/daily-summary';
